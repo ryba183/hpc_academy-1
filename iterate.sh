307af6d9089ece69f99971ae7e20eac1f523a04e
Stephen Richer <sr467@bath.ac.uk>
2019-09-06 08:00:57 +0100

    Updated

--- iterate.sh
+++ iterate.sh
@@ -15,6 +15,6 @@ function iterate {
 
 export -f iterate
 
-binrange=( $(seq 101 100 1000) $(seq 1001 1000 10000) $(seq 10001 10000 110000) $(seq 100001 100000 1100000) )
-
-parallel -j 12 iterate ::: "${binrange[@]}"
+#binrange=( $(seq 101 100 1000) $(seq 1001 1000 10000) $(seq 10001 10000 110000) $(seq 100001 100000 1100000) )
+binrange=( 101 501 1001 5001 10001 50001 100001 500001 1000001 )
+parallel -j 1 iterate ::: "${binrange[@]}"
